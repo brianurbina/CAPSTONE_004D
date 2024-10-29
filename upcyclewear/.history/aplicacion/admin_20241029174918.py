b34268ee980bf090945ee6dc85d9bbca7e63115c
Bria.urbina <bria.urbina@duocuc.cl>
2024-10-29 20:47:33 -0300

    Reportes, perfil usuario listo, reseña casi lista
    Co-Authored-By: MaximilianoBlamey <80706950+Draigonot@users.noreply.github.com>

---
+++ upcyclewear/.history/aplicacion/admin_20241029174918.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import (
+    Usuario, Donacion, HistorialDonaciones, Conversacion, 
+    Mensaje, Reporte, Fundacion, Peticion, Reseña, HistorialPeticiones,ReporteUsuario
+)
+
+admin.site.register(Usuario)
+admin.site.register(Donacion)
+admin.site.register(HistorialDonaciones)
+admin.site.register(Conversacion)
+admin.site.register(Mensaje)
+admin.site.register(Reporte)
+admin.site.register(Fundacion)
+admin.site.register(Peticion)
+admin.site.register(HistorialPeticiones)
+admin.site.register(Reseña)
+admin.site.register(ReporteUsuario)
